953c0b476badff7afb280533450efb36e413a803
Julian Burner <burnerjulian@gmail.com>
2022-02-13 18:26:42 +0100

    Use Lombok's getter and setter annotation for data classes

--- src/main/java/com/github/reviversmc/modget/library/data/ModUpdate.java
+++ src/main/java/com/github/reviversmc/modget/library/data/ModUpdate.java
@@ -5,8 +5,12 @@ import java.util.List;
 import com.github.reviversmc.modget.manifests.spec4.api.data.manifest.version.ModVersionVariant;
 import com.github.reviversmc.modget.manifests.spec4.api.data.mod.InstalledMod;
 
+import lombok.Getter;
 import lombok.NonNull;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class ModUpdate {
 	private final InstalledMod installedMod;
 	private List<ModVersionVariant> latestModVersionVariants;
@@ -16,19 +20,6 @@ public class ModUpdate {
 			@NonNull InstalledMod installedMod,
 			@NonNull List<ModVersionVariant> latestModVersionVariants) {
 		this.installedMod = installedMod;
-		this.latestModVersionVariants = latestModVersionVariants;
-	}
-
-
-	public InstalledMod getInstalledMod() {
-		return this.installedMod;
-	}
-
-	public List<ModVersionVariant> getLatestModVersionVariants() {
-		return this.latestModVersionVariants;
-	}
-
-	public void setLatestModVersionVariants(List<ModVersionVariant> latestModVersionVariants) {
 		this.latestModVersionVariants = latestModVersionVariants;
 	}
 
